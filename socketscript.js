15c6aeef57897b60bb65d3b1a49425c94d3e4c19
lb321 <lucas.001@live.nl>
2017-11-16 13:07:54 +0100

    datum in filename en logged ook signaal sterkte

--- socketscript.js
+++ socketscript.js
@@ -6,67 +6,66 @@ var raspberryPi_Lift = [];
 var socket = require('socket.io-client')('http://localhost/scanner');
 
 //replace with your hardware address
-var addressToTrack = '623bc0d1cffb'; 
+var addressToTrack = '623bc0d1cffb';
 
-socket.on('connect', function(){  
-  console.log('connected to server');
+socket.on('connect', function(){
+    console.log('connected to server');
 });
 
 noble.on('discover', function(peripheral){
-  if(peripheral.advertisement.localName && peripheral.advertisement.localName.indexOf('BlueUp') != -1){
-  //console.log('peripheral: ' + peripheral);
-  //console.log(peripheral.uuid + ', '+ peripheral.rssi);
-  //socket.emit('deviceData', {mac: peripheral.uuid, rssi:peripheral.rssi});
-
-console.log('Beacon ' + peripheral.advertisement.localName + ',' + 'found at time: ' + timeconverter(Date.now()));
-  raspberryPi_Lift.push('Beacon ' + peripheral.advertisement.localName + ',' + 'found at time: ' + timeconverter(Date.now()) + '\n\r');
-  if (raspberryPi_Lift.length >= 5) {
-    fs.writeFile('./lijst.csv',raspberryPi_Lift.join(''),{flag:'a'});
-    raspberryPi_Lift = [];
-  } 
-  //console.log(raspberryPi_Lift);
-  //console.log('distance: ' + calculateDistance(peripheral.rssi));
-  }
+    if(peripheral.advertisement.localName && peripheral.advertisement.localName.indexOf('BlueUp') != -1){
+        console.log('Beacon ' + peripheral.advertisement.localName + ',' + 'found at time: ' + timeconverter(Date.now()));
+        //raspberryPi_Lift.push('Beacon ' + peripheral.advertisement.localName + ',' + 'found at time: ' + timeconverter(Date.now()) + '\n\r');
+        raspberryPi_Lift.push(peripheral.advertisement.localName + ',' + timeconverter(Date.now()) + ',' + peripheral.rssi + '\n\r');
+        if (raspberryPi_Lift.length >= 5) {
+            writeArray(raspberryPi_Lift);
+            raspberryPi_Lift = [];
+        }
+        //console.log('distance: ' + calculateDistance(peripheral.rssi));
+    }
 });
 
 noble.on('stateChange', function(state) {
-	if(state == 'poweredOn'){
-		noble.startScanning([], true) //allows dubplicates while scanning
-	} else {
-		noble.stopScanning();
-	}
+    if(state == 'poweredOn'){
+        noble.startScanning([], true) //allows dubplicates while scanning
+    } else {
+        noble.stopScanning();
+    }
 });
 
 function writeArray(array) {
-	
+    var currentDate = Date.now();
+    var fileName = './lijst' + currentDate.getDate() + '-' + currentDate.getMonth() + '-' + currentDate.getYear() + '.csv';
+    //fs.writeFile('./lijst.csv',raspberryPi_Lift.join(''),{flag:'a'});
+    fs.appendFile(fileName, array);//  append data to a file, creating the file if it does not yet exist
 }
 
 function calculateDistance(rssi) {
-  
-  var txPower = -59 //hard coded power value. Usually ranges between -59 to -65
-  
-  if (rssi == 0) {
-    return -1.0; 
-  }
 
-  var ratio = rssi*1.0/txPower;
-  if (ratio < 1.0) {
-    return Math.pow(ratio,10);
-  }
-  else {
-    var distance =  (0.89976)*Math.pow(ratio,7.7095) + 0.111;    
-    return distance * 0.3048;
-  }
-} 
+    var txPower = -59 //hard coded power value. Usually ranges between -59 to -65
+
+    if (rssi == 0) {
+        return -1.0;
+    }
+
+    var ratio = rssi*1.0/txPower;
+    if (ratio < 1.0) {
+        return Math.pow(ratio,10);
+    }
+    else {
+        var distance =  (0.89976)*Math.pow(ratio,7.7095) + 0.111;
+        return distance * 0.3048;
+    }
+}
 
 function timeconverter(UNIX_timestamp){
-  var a = new Date(UNIX_timestamp );
-  var year = a.getFullYear();
-  var month = a.getMonth() + 1 ;
-  var date = a.getDate();
-  var hour = a.getHours();
-  var min = a.getMinutes();
-  var sec = a.getSeconds();
-  var time = date + '-' + month + '-' + year + ',' + hour + ':' + min + ':' + sec ;
-  return time;
+    var a = new Date(UNIX_timestamp );
+    var year = a.getFullYear();
+    var month = a.getMonth() + 1 ;
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    var time = date + '-' + month + '-' + year + ',' + hour + ':' + min + ':' + sec ;
+    return time;
 }
